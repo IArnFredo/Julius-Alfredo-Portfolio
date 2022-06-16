1eb54fa7377b0c0535df54682facca87a833fbd1
Julius Alfredo <ju.27.al@gmail.com>
2022-06-16 13:17:28 +0700

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,112 @@
+import { useRef } from "react";
+import Link from "next/link";
+import Header from "../components/Header";
+import ServiceCard from "../components/ServiceCard";
+import Socials from "../components/Socials";
+import WorkCard from "../components/WorkCard";
+import Head from 'next/head'
+// Local Data
+import data from "../yourData";
+
+export default function Home() {
+  // Ref
+  const workRef = useRef();
+  const aboutRef = useRef();
+  const contactRef = useRef();
+
+  // Handling Scroll
+  const handleWorkScroll = () => {
+    window.scrollTo({
+      top: workRef.current.offsetTop,
+      left: 0,
+      behavior: "smooth",
+    });
+  };
+
+  const handleAboutScroll = () => {
+    window.scrollTo({
+      top: aboutRef.current.offsetTop,
+      left: 0,
+      behavior: "smooth",
+    });
+  };
+
+  const handleContactScroll = () => {
+    window.scrollTo({
+      top: contactRef.current.offsetTop,
+      left: 0,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    <div className="container mx-auto mb-10">
+      <Head>
+        <title>Julius Alfredo - Portfolio</title>
+        <link rel="icon" href="https://i.ibb.co/GQqLqnr/Julius-Alfredo-1.png" />
+      </Head>
+      <Header
+        handleWorkScroll={handleWorkScroll}
+        handleAboutScroll={handleAboutScroll}
+        handleContactScroll={handleContactScroll}
+      />
+      <div className="laptop:mt-20 mob:mt-10">
+        <div className="columns-1">
+          <h1 className="mt-5 text-8xl mob:text-3xl laptop:text-8xl mob:p-2 text-bold w-4/5 mob:w-full laptop:w-4/5">
+            {data.headerTaglineOne} <br />
+            {data.headerTaglineTwo}
+          </h1>
+          <Socials className="mt-5 mob:mt-2 laptop:mt-5" />
+          <img src={data.headerImage} className="mt-5" width="300vw" ref={aboutRef} />
+        </div>
+        <div
+          className="mt-40 mob:mt-2 laptop:mt-40 mob:p-2 laptop:p-0"
+        >
+          <h1 className="text-2xl text-bold">About.</h1>
+          <p className="m-5 mob:m-0 laptop:m-5 mob:mt-2 laptop:ml-0 ml-0 text-3xl mob:text-xl laptop:text-3xl w-3/5 mob:w-full laptop:w-3/5">
+            {data.aboutpara}
+          </p>
+        </div>
+      </div>
+      <div
+        className="mt-40 mob:mt-10 laptop:mt-40 mob:p-2 laptop:p-0"
+        ref={workRef}
+      >
+        <h1 className="text-2xl text-bold">Projects.</h1>
+        <div className="mt-10 mob:mt-5 laptop:mt-10 grid grid-cols-2 mob:grid-cols-1 laptop:grid-cols-2 gap-4">
+          {data.projects.map((project, index) => (
+            <WorkCard
+              key={index}
+              img={project.imageSrc}
+              name={project.title}
+              description={project.description}
+              onClick={() => window.open(project.url)}
+            />
+          ))}
+        </div>
+      </div>
+      <div className="mt-40 mob:mt-2 laptop:mt-40 mob:p-2 laptop:p-0">
+        <h1 className="text-2xl text-bold">Specialist.</h1>
+        <div className="mt-10 grid grid-cols-2 mob:grid-cols-1 laptop:grid-cols-2 gap-6">
+          {data.services.map((service, index) => (
+            <ServiceCard
+              key={index}
+              name={service.title}
+              description={service.description}
+            />
+          ))}
+        </div>
+      </div>
+
+      <div className="mt-40 mob:mt-5 laptop:mt-40 mob:p-2 laptop:p-0" ref={contactRef}>
+        <h1 className="text-2xl text-bold">Contact.</h1>
+        <div className="mt-5">
+          <Socials />
+        </div>
+      </div>
+      <h1 className="text-sm text-bold mt-10 mob:mt-2 laptop:mt-10 mob:p-2 laptop:p-0">
+        Made With ❤ by Julius Alfredo ©2022
+      </h1>
+    </div>
+  );
+}
---
+++ yourData.js
@@ -0,0 +1,89 @@
+export default {
+  //(Please Do Not Remove The comma(,) after every variable)
+  //Change The Website Template
+
+  //   Header Details ---------------------
+  name: "chetan",
+  headerTaglineOne: "Hello 👋",
+  headerTaglineTwo:
+    "I'm Julius Alfredo - curious Web developer based in Tangerang, Indonesia. ",
+  headerImage: "https://i.ibb.co/716H929/Julius-Alfredo-Informatika-Foto.jpg",
+
+  socials: [
+    { title: "Github", link: "https://github.com/IArnFredo" },
+    {
+      title: "LinkedIn",
+      link: "https://www.linkedin.com/in/julius-alfredo-6a37451b9/",
+    },
+    { title: "Email", link: "mailto:julius.alfredo@student.umn.ac.id" },
+    { title: "Instagram", link: "https://www.instagram.com/alfred_oj/" },
+  ],
+
+  // End Header Details -----------------------
+
+  // Work Section ------------------------
+  projects: [
+    {
+      title: "Djongers Weather", //Project Title - Add Your Project Title Here
+      description: "Web used to track the weather around the world",
+      //Project Image - Add Your Project Image Here
+      imageSrc: "https://i.ibb.co/JRDjJBp/project1.png",
+      //Project URL - Add Your Project Url Here
+      url: "https://djong-weather.vercel.app/",
+    },
+    {
+      title: "SeiYou", //Project Title - Add Your Project Title Here
+      description:
+        "Mobile Cross Platform themed social media that people use to post their voice acting",
+      //Project Image - Add Your Project Image Here
+      imageSrc: "https://i.ibb.co/Gv6BtvR/project3.png",
+      //Project URL - Add Your Project Url Here
+      url: "https://sei-you.vercel.app/",
+    },
+    {
+      title: "Endah Karya Mandiri", //Project Title - Add Your Project Title Here
+      description: "PT. Endah Karya Mandiri is a company engaged in the delivery of goods by land, sea, air and logistics in a professional manner to answer the challenges of fast, accurate and safe delivery services.",
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://i.ibb.co/rZznLR0/project2.png",
+      //Project URL - Add Your Project Url Here
+      url: "https://endahkaryamandiri.000webhostapp.com/",
+    },
+
+    /*
+    If You Want To Add More Project just Copy and Paste This At The End
+    ,{
+        title: 'Project Five',
+        description: 'Something Amazing',
+        imageSrc: "",
+        url: ''
+    }
+    */
+  ],
+
+  // End Work Section -----------------------
+
+  // Services Section ---------------
+
+  services: [
+    {
+      title: "Web Developer",
+      description:
+        "I am helping with the creation and development of Web Development with particular focus on their visual appearance and great function (ReactJS, NextJS, Ionic React, Laravel).",
+    },
+    {
+      title: "Web Design",
+      description:
+        "I do build and optimize your online presence.  Website is the digital entry point into your business and a powerful revenue channel.",
+    },
+  ],
+
+  // End Services Section --------------------------
+
+  // About Secton --------------
+  aboutpara:
+    "I am someone who is very interested in the world of Informatics, especially in the field of the Web development. Therefore, I am very happy to implement my knowledge in manufacturing The web and I are sociable and adapt easily to new environments.",
+  //   End About Section ---------------------
+};
+
+// Thanks for using this template, I would love to hear from you contact me at hello@chetanverma.com
